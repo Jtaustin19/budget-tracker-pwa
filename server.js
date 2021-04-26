207fa6b8f8a0b617f201f744bc972faaf59750fe
Josh <jtaustin19@icloud.com>
2021-04-25 23:00:44 -0500

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-pwa";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-cache-v1";
 
 const app = express();
 
